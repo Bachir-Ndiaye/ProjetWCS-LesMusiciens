f36e7f8486a28c0d75166ca4a40126c6f813cbcb
Bachir-Ndiaye <amadou.n-diaye@hotmail.com>
2021-03-16 23:24:45 +0100

    finished footer.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--Some basic meta tags-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--CSS links-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./src/css/mobile-first.css">
+
+    <!--Page title-->
+    <title>Acceuil - findXlyon </title>
+</head>
+<body>
+    <!--Here header.php-->
+
+    <!-- Here Carrousel/Hero-->
+
+    <!-- Here Events-->
+
+    <!--Last subscribers-->
+
+    <!-- Post announce or search for a groupe-->
+
+    <!--Winner of the week (coup de coeur) -->
+
+    <!--Clients comments about musicians-->
+    
+    <!--FOOTER-->
+    <?php require './src/php/footer.php'; ?>
+</body>
+</html>
---
+++ src/php/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="basic-footer">
+        <i class="fa fa-copyright"></i>
+        <h4>findXlyon</h4>
+    </div>
+</footer>
