7ea0b9549f74052c2abd958fcf9083d21ae3fd81
benoit <cousinbenoit@hotmail.fr>
2021-03-17 16:07:11 +0100

    ajout des photos dans index.php

--- index.php
+++ index.php
@@ -18,21 +18,20 @@
 </head>
 <body>
     <!--Here header.php-->
-<<<<<<< HEAD
     <?php require './src/php/header.php'; ?>
-    <!-- Here Carrousel/Hero-->
-=======
+    
 
     <!-- Here slider-->
     <div id="slider">
         <figure>
             <img src="https://cdn.pixabay.com/photo/2018/06/30/09/29/music-3507317_960_720.jpg"/>
             <img src="https://cdn.pixabay.com/photo/2015/05/07/11/02/guitar-756326_960_720.jpg"/>
+            <img src="https://cdn.pixabay.com/photo/2017/06/06/22/58/face-2378774_960_720.jpg"/>
+            <img src="https://cdn.pixabay.com/photo/2018/06/17/10/38/artist-3480274_960_720.jpg"/>
             <img src="https://cdn.pixabay.com/photo/2016/07/20/22/15/crowd-1531431_960_720.jpg"/>
         </figure>
         </div>
     <!-- slider -->
->>>>>>> benoit
 
     <!-- Here Events-->
     <div class="events">
