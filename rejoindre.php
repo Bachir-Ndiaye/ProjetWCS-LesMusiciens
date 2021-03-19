22375d4f8c29e2920ae0b8b983c095763dda846d
benoit <cousinbenoit@hotmail.fr>
2021-03-19 15:06:52 +0100

    création de la page rejoignez-nous (base) prochaine étape le css

---
+++ rejoindre.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/css/mobile-first.css">
+    <title>Document</title>
+
+</head>
+<body>
+<?php require './src/php/header.php'; ?>
+
+   <div>
+    <article class="valeur">
+        <h1>Nos valeurs</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, quia. 
+        Odio voluptas, id nemo, accusantium omnis necessitatibus atque assumenda amet nam nisi sed delectus illum dolorum similique doloremque ex, 
+        aspernatur laborum mollitia alias dignissimos? Id officia qui ea eaque rerum fuga quasi. 
+        Facilis corporis doloribus unde modi blanditiis, sint quam.</p>
+    </article>
+
+    <article clas="projet">
+        <h2>Nos projets</2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, quia. 
+        Odio voluptas, id nemo, accusantium omnis necessitatibus atque assumenda amet nam nisi sed delectus illum dolorum similique doloremque ex, 
+        aspernatur laborum mollitia alias dignissimos? Id officia qui ea eaque rerum fuga quasi. 
+        Facilis corporis doloribus unde modi blanditiis, sint quam.</p>
+    </article>
+
+    <article class="">
+        <h3>Tarifs</3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, quia. 
+        Odio voluptas, id nemo, accusantium omnis necessitatibus atque assumenda amet nam nisi sed delectus illum dolorum similique doloremque ex, 
+        aspernatur laborum mollitia alias dignissimos? Id officia qui ea eaque rerum fuga quasi. 
+        Facilis corporis doloribus unde modi blanditiis, sint quam.</p>
+    </article>
+    </div>
+
+<?php require './src/php/footer.php'; ?>
+<script type="text/javascript" src="./src/js/header.js"></script>
+</body>
+</html>
