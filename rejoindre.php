35739a16cffa9ba5e3df9bc6dc6b7c624645bbd8
benoit <cousinbenoit@hotmail.fr>
2021-03-20 10:56:39 +0100

    premier rendu de la page rejoignez-nous

--- rejoindre.php
+++ rejoindre.php
@@ -10,32 +10,31 @@
 </head>
 <body>
 <?php require './src/php/header.php'; ?>
-
-   <div>
-    <article class="valeur">
-        <h1>Nos valeurs</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, quia. 
-        Odio voluptas, id nemo, accusantium omnis necessitatibus atque assumenda amet nam nisi sed delectus illum dolorum similique doloremque ex, 
-        aspernatur laborum mollitia alias dignissimos? Id officia qui ea eaque rerum fuga quasi. 
-        Facilis corporis doloribus unde modi blanditiis, sint quam.</p>
-    </article>
-
-    <article clas="projet">
-        <h2>Nos projets</2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, quia. 
-        Odio voluptas, id nemo, accusantium omnis necessitatibus atque assumenda amet nam nisi sed delectus illum dolorum similique doloremque ex, 
-        aspernatur laborum mollitia alias dignissimos? Id officia qui ea eaque rerum fuga quasi. 
-        Facilis corporis doloribus unde modi blanditiis, sint quam.</p>
-    </article>
-
-    <article class="">
-        <h3>Tarifs</3>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, quia. 
-        Odio voluptas, id nemo, accusantium omnis necessitatibus atque assumenda amet nam nisi sed delectus illum dolorum similique doloremque ex, 
-        aspernatur laborum mollitia alias dignissimos? Id officia qui ea eaque rerum fuga quasi. 
-        Facilis corporis doloribus unde modi blanditiis, sint quam.</p>
-    </article>
+  
+<div class="container">
+  <div class="card">
+     <div class="card__image-container">
+       <img class="card__image" src="https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2126&q=80" alt="">
     </div>
+      
+      <svg class="card__svg" viewBox="0 0 800 500">
+
+        <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"/>
+        <path class="card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" stroke-width="3" fill="transparent"/>
+      </svg>
+    
+     <div class="card__content">
+       <h1 class="card__title">Nos valeurs</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor praesentium at quod autem omnis, amet eaque unde perspiciatis adipisci possimus quam facere illo et quisquam quia earum nesciunt porro.</p>
+    </div>
+
+
+
+
+    
+  </div>
+</div>
+
 
 <?php require './src/php/footer.php'; ?>
 <script type="text/javascript" src="./src/js/header.js"></script>
