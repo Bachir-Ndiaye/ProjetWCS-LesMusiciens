d4e4e433b6a04fef760c3ec0c45c307824f4e7e5
benoit <cousinbenoit@hotmail.fr>
2021-03-21 10:56:00 +0100

    correction des classes sur img

--- blog.php
+++ blog.php
@@ -26,7 +26,7 @@
 <div class="picture-all">
     <div class="picture">  
             <div class="identity-picture-one">
-                <img src="/src/img/backshot-hipster.jpg" alt="musicien sur la plage">
+                <img class="imgblog" src="/src/img/backshot-hipster.jpg" alt="musicien sur la plage">
                 <div class="texte-picture">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro facere delectus ad nostrum! Ut dignissimos nisi, dolor, debitis cupiditate expedita saepe quae quaerat ullam tempora explicabo quisquam obcaecati eligendi cumque. Eveniet illum ut animi ad accusantium, voluptatem obcaecati maxime deleniti cupiditate consequatur, excepturi pariatur rerum porro! Dolorem vitae voluptates eum laboriosam magni! Dolores distinctio fuga, a dolorem nam voluptatibus nobis aut rerum, expedita consequatur voluptatem. Labore, praesentium! Adipisci eaque, nam sint ipsum, eius totam ad pariatur tenetur quam atque illum! </p>
                     <h4>Publié le: 19/11/2020 par </h4>
@@ -38,7 +38,7 @@
 
     <div class="picture">
             <div class="identity-picture-two">
-                <img src="/src/img/batteur-professionnel.jpg" alt="batteur ">
+                <img class="imgblog" src="/src/img/batteur-professionnel.jpg" alt="batteur ">
                 <div class="texte-picture">
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis id tempora soluta alias adipisci ex, et, quisquam dolores eveniet doloremque harum dolorem consequuntur non quod nihil provident quas voluptatibus nam nobis dolor labore! Placeat neque, aliquam laborum sequi, repudiandae eveniet quam dolores reprehenderit animi ipsam corporis incidunt? Ea, assumenda tempora architecto, repellendus corrupti possimus culpa placeat cupiditate unde in blanditiis voluptatibus! Minima accusamus doloribus excepturi, ab suscipit totam adipisci exercitationem nulla nisi voluptatum. Praesentium quasi sit voluptate ipsum obcaecati iste! </p>
                      <h4>Publié le: 1/03/2020 par</h4>
@@ -50,7 +50,7 @@
 
     <div class="picture">
             <div class="identity-picture-tree">
-                <img src="/src/img/deux-rappeurs.jpg" alt="deux rapeur">
+                <img class="imgblog" src="/src/img/deux-rappeurs.jpg" alt="deux rapeur">
                 <div class="texte-picture">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ad reiciendis sunt quae, quo suscipit tempore facilis deserunt ut atque nihil laborum, non exercitationem? Qui architecto perferendis nihil sunt sint commodi vel, dolorum laboriosam nesciunt. Temporibus, eum. Facilis repellat iure quis ad, atque laudantium, cum quisquam necessitatibus maiores odio laborum velit rerum tenetur quam officia, iusto sed vitae. Ut voluptates illo eveniet magni reprehenderit? Illum, odio, atque et, consequuntur quas corrupti quae architecto pariatur molestias facere tempore repellat ducimus eius. </p>
                     <h4>Publié le: 10/09/2020 par</h4>
@@ -63,7 +63,7 @@
 
     <div class="picture">
             <div class="identity-picture-four">
-                <img src="/src/img/fille-punk-vetue-pantalon-cuir.jpg" alt="fille punk">
+                <img class="imgblog" src="/src/img/fille-punk-vetue-pantalon-cuir.jpg" alt="fille punk">
                 <div class="texte-picture">
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore corporis animi provident laboriosam molestiae. Magnam, ullam debitis unde sunt perspiciatis, laborum ducimus eius quaerat odio aliquam culpa facilis veniam reiciendis dolor dicta impedit ea rem iure numquam distinctio. Voluptates possimus adipisci laboriosam? Ipsam ab, debitis assumenda iste modi saepe aliquid doloribus blanditiis exercitationem, earum reprehenderit est dignissimos sed libero placeat delectus eaque alias laborum ex consequuntur mollitia officia fugiat reiciendis? Sit nobis explicabo, eius iusto id quo mollitia totam ex.</p>
                     <h4>Publié le: 13/01/2020 par</h4>
@@ -75,7 +75,7 @@
     
     <div class="picture">
             <div class="identity-picture-five">
-                <img src="/src/img/jeunes-gens-danser.jpg" alt="musicien classic">
+                <img class="imgblog" src="/src/img/jeunes-gens-danser.jpg" alt="musicien classic">
                 <div class="texte-picture">
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia consequatur hic est commodi aut ipsum quibusdam corrupti nam quaerat quia dignissimos magnam aspernatur, id, assumenda perferendis saepe voluptas impedit omnis ipsam repudiandae? Tenetur eveniet vel provident temporibus doloribus vero illo, aliquam sit cumque repellat ipsum perspiciatis in expedita enim! Natus cum autem incidunt quo nihil fugit, explicabo aliquam. Fugit deserunt nisi eos omnis reprehenderit, quaerat nulla id quae aperiam at ipsam sit iste soluta vel placeat neque ratione illum officiis!</p>
                     <h4>Publié le: 14/01/2021 par</h4>
@@ -87,7 +87,7 @@
 
     <div class="picture">
             <div class="identity-picture-six">
-                <img src="/src/img/musiciens-rap.jpg" alt="musicien hip hop">
+                <img class="imgblog" src="/src/img/musiciens-rap.jpg" alt="musicien hip hop">
                 <div class="texte-picture">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab sunt perspiciatis explicabo? Adipisci architecto explicabo praesentium dolorum. Nesciunt enim accusantium ut alias molestias iure maxime deleniti numquam ipsa! Ratione recusandae consequuntur natus assumenda nesciunt nostrum suscipit unde, quaerat possimus vel quia iure dicta qui dolor temporibus reprehenderit voluptate quisquam reiciendis! Deleniti atque unde, vitae ipsum inventore veritatis expedita, voluptatum porro minima culpa nulla impedit animi laborum obcaecati dolore repudiandae ea praesentium adipisci corrupti rem optio. Excepturi fugit cum enim iure? </p>
                     <h4>Publié le: 17/02/2021 par</h4>
